f4ec63cba6414fe2379dbf553ff6a9df2b46f9b1
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2025-01-26 11:23:06 +0200

    Making Microservices Resilient using Resilience4j

--- section_10/accounts/src/main/java/com/roaa/accounts/service/client/LoansFeignClient.java
+++ section_10/accounts/src/main/java/com/roaa/accounts/service/client/LoansFeignClient.java
@@ -6,12 +6,13 @@ import com.roaa.accounts.dto.LoansDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("loans")
+@FeignClient(name="loans", fallback = LoansFallback.class)
 public interface LoansFeignClient {
 
     @GetMapping(value = "/api/fetch", consumes = "application/json")
     public ResponseEntity<LoansDto> fetchLoanDetails(
-            @RequestParam String correlationId, @RequestParam String mobileNumber);
+            @RequestHeader("roaabank-correlation-id") String correlationId, @RequestParam String mobileNumber);
 }
