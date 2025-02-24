5a090cc9eab4f72668dc3ed22684ce6c77b227fb
ahmedabdellatifss <ahmedabdellatifss@yahoo.com>
2025-02-24 12:05:30 +0200

    Event Driven microservices using Kafka,Spring Cloud Functions & Stream

---
+++ section_14/accounts/src/main/java/com/roaa/accounts/service/impl/CustomersServiceImpl.java
@@ -0,0 +1,58 @@
+package com.roaa.accounts.service.impl;
+
+import com.roaa.accounts.dto.AccountsDto;
+import com.roaa.accounts.dto.CardsDto;
+import com.roaa.accounts.dto.CustomerDetailsDto;
+import com.roaa.accounts.dto.LoansDto;
+import com.roaa.accounts.entity.Accounts;
+import com.roaa.accounts.entity.Customer;
+import com.roaa.accounts.exception.ResourceNotFoundException;
+import com.roaa.accounts.mapper.AccountsMapper;
+import com.roaa.accounts.mapper.CustomerMapper;
+import com.roaa.accounts.repository.AccountRepository;
+import com.roaa.accounts.repository.CustomerRepository;
+import com.roaa.accounts.service.ICustomersService;
+import com.roaa.accounts.service.client.CardsFeignClient;
+import com.roaa.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements ICustomersService {
+
+    private AccountRepository accountRepository;
+    private CustomerRepository customerRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId) {
+
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Accounts accounts = accountRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "CustomerId", customer.getCustomerId().toString())
+        );
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationId, mobileNumber);
+        if(null != loansDtoResponseEntity) {
+            customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        }
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(correlationId,mobileNumber);
+        if(null != cardsDtoResponseEntity) {
+            customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        }
+
+        return customerDetailsDto;
+    }
+}
